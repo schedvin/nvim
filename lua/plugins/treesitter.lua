c42cf02e0b794f28c377fddbc41dda033ef21853
Carl Johan Schedvin <carl.johan.schedvin@dlaboratory.com>
2023-09-14 19:26:19 +0200

    feat: ensure treesitter parsers are installed

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,14 +1,14 @@
 return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
-    config = function () 
+    config = function ()
       local configs = require("nvim-treesitter.configs")
 
       configs.setup({
-          ensure_installed = { "c_sharp", "html", "json", "lua", "markdown", "toml", "tsx", "typescript", "yaml" },
+          ensure_installed = { "c_sharp", "html", "http", "json", "lua", "markdown", "toml", "tsx", "typescript", "yaml" },
           sync_install = false,
           highlight = { enable = true },
-          indent = { enable = true },  
+          indent = { enable = true },
         })
     end
 }
